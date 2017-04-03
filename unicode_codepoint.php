89390be0ca94f8c617ab6a24572421ed44db8d56
Oleg Stakhiv <veishen19@gmail.com>
2017-04-03 22:03:23 +0300

    unicode codepoint

---
+++ unicode_codepoint.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo "&pound;";
+    echo "&amp;";
+
+    /** \u means we want a unicode escaped character */
+    /** https://www.w3schools.com/charsets/ref_html_utf8.asp */
+    echo "\u{2764}";
+    echo "\u{2200}";
+    echo "\u{221C}";
