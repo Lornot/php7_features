b3ea02f819e7be43c5d7e9f4759a76b0ebb49c9b
Oleg Stakhiv <veishen19@gmail.com>
2017-04-03 22:38:03 +0300

    unserialize filtering

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
 <?php
+    error_reporting(E_ALL);
 
+    class newObj {
+
+        public $prop = "hello world";
+
+        private $priv = "This is private to the object";
+
+        private $arr = [ 20, 200, 500, 600 ];
+
+    }
+
+    $data = serialize( new newObj() );
+
+    //print_r($data);
+
+    $unserialized = unserialize($data, ['allowed_classes' => ['newObj', 'DateTime']]);
+
+    echo '<br/><br/>'. var_export($unserialized);
